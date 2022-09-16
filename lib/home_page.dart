def2580fb752fd04a909026bdf41cca7741180a2
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2022-09-16 15:52:06 +0700

    google ml kit on camera image stream

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,10 @@
-import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:rxdart/rxdart.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -20,11 +20,56 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late CameraController _cameraController;
+  late BehaviorSubject<Face> _faceStream;
 
   bool _cameraInitialized = false;
 
   Future<XFile> _capture() async {
     return _cameraController.takePicture();
+  }
+
+  InputImage _getInputImage(CameraImage image) {
+    var allBytes = WriteBuffer();
+    for (var plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    var bytes = allBytes.done().buffer.asUint8List();
+
+    var imageSize = Size(image.width.toDouble(), image.height.toDouble());
+
+    var imageRotation = InputImageRotationValue.fromRawValue(
+        _cameraController.description.sensorOrientation);
+
+    var imageFormat = InputImageFormatValue.fromRawValue(image.format.raw);
+
+    var planeData = image.planes
+        .map(
+          (plane) => InputImagePlaneMetadata(
+            bytesPerRow: plane.bytesPerRow,
+            width: plane.width,
+            height: plane.height,
+          ),
+        )
+        .toList();
+
+    var imageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: imageFormat!,
+      planeData: planeData,
+    );
+
+    return InputImage.fromBytes(bytes: bytes, inputImageData: imageData);
+  }
+
+  void _processImage(CameraImage image) async {
+    var faceDetector = FaceDetector(
+      options: FaceDetectorOptions(enableClassification: true),
+    );
+    var faces = await faceDetector.processImage(_getInputImage(image));
+    if (faces.isNotEmpty) {
+      _faceStream.add(faces[0]);
+    }
   }
 
   void _initializeCamera() async {
@@ -39,6 +84,7 @@ class _HomePageState extends State<HomePage> {
             .lockCaptureOrientation(DeviceOrientation.landscapeLeft);
       }
       setState(() {
+        _cameraController.startImageStream(_processImage);
         _cameraInitialized = true;
       });
     });
@@ -47,12 +93,15 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     _initializeCamera();
+    _faceStream = BehaviorSubject()
+      ..debounceTime(const Duration(milliseconds: 500));
     super.initState();
   }
 
   @override
   void dispose() {
     _cameraController.dispose();
+    _faceStream.close();
     super.dispose();
   }
 
@@ -62,47 +111,92 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _cameraInitialized
-          ? ClipRect(
-              child: OverflowBox(
-                alignment: Alignment.center,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height /
-                        _cameraController.value.aspectRatio,
-                    child: AspectRatio(
-                      aspectRatio: _cameraController.value.aspectRatio,
-                      child: CameraPreview(_cameraController),
+      body: Stack(
+        children: [
+          _cameraInitialized
+              ? ClipRect(
+                  child: OverflowBox(
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height /
+                            _cameraController.value.aspectRatio,
+                        child: AspectRatio(
+                          aspectRatio: _cameraController.value.aspectRatio,
+                          child: CameraPreview(_cameraController),
+                        ),
+                      ),
                     ),
                   ),
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
                 ),
+          Positioned(
+            top: 20,
+            left: MediaQuery.of(context).size.width / 2 - 110 / 2,
+            child: Container(
+              width: 110,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(12),
               ),
-            )
-          : const Center(child: CircularProgressIndicator()),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (_cameraInitialized) {
-            var image = await _capture();
-            var faceDetector = FaceDetector(
-              options: FaceDetectorOptions(enableClassification: true),
-            );
-            var faces = await faceDetector
-                .processImage(InputImage.fromFilePath(image.path));
-            var result = 'no face detected';
-            if (faces.isNotEmpty) {
-              result =
-                  'Left Eye: ${faces[0].leftEyeOpenProbability}, Right Eye: ${faces[0].rightEyeOpenProbability}, Smile: ${faces[0].smilingProbability}';
-            }
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text(result)));
-          }
-        },
-        tooltip: 'Capture',
-        child: const Icon(Icons.camera_alt),
+              child: const Text(
+                'Blink your eyes',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: MediaQuery.of(context).size.width / 2 - 170 / 2,
+            child: Container(
+              width: 170,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: StreamBuilder<Face>(
+                stream: _faceStream,
+                builder: (context, snapshot) {
+                  return Column(
+                    children: [
+                      Text(
+                        'Left Eye Probability: ${snapshot.hasData ? snapshot.data!.leftEyeOpenProbability : '-'}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        'Right Eye Probability: ${snapshot.hasData ? snapshot.data!.rightEyeOpenProbability : '-'}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        'Smiling Probability: ${snapshot.hasData ? snapshot.data!.smilingProbability : '-'}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
